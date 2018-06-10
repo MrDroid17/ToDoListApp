ef0ccf00f05742783ad8e530a8bcd70c2d62e57a
Sobhit Kumar <sobhit2010@gmail.com>
2018-06-11 00:20:40 +0530

    Add TaskDatabase Class

---
+++ app/src/main/java/com/example/android/todolist/database/TaskDatabase.java
@@ -0,0 +1,37 @@
+package com.example.android.todolist.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
+
+@Database(entities = {TaskEntry.class}, version = 1, exportSchema = false)
+public abstract class TaskDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = TaskDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "todolist";
+    private static final Object LOCK = new Object();
+    private static TaskDatabase ourInstance;
+
+    public static TaskDatabase getInstance(Context context) {
+        if (ourInstance == null) {
+            synchronized (LOCK){
+                Log.d(LOG_TAG, "Creating new Database Instance");
+
+                ourInstance = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        TaskDatabase.class,
+                        TaskDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+
+        Log.d(LOG_TAG, "getInstance: Getting database instance");
+        return ourInstance;
+    }
+
+    private TaskDatabase() {
+    }
+}
